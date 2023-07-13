dde7158f08cd96d43c7998420057a515aca05c08
jitendra <jitendrasaini352jjnu@gmail.com>
2023-07-13 23:52:42 +0530

    variables

---
+++ variable-and-data-structure/JavaBasics.java
@@ -0,0 +1,12 @@
+public class JavaBasics{
+    public static void main(String args[]){
+        System.out.println("JavaBasics is my first java output");
+        System.out.print("JavaBasics is my second java output \n");
+        System.out.println("JavaBasics is my  third java output");
+        System.out.println("JavaBasics is my fourth java output");
+         int i = 5;
+         int j = 10;
+         int sum = i + j;
+         System.out.println("sum = " + sum);
+    }
+}
